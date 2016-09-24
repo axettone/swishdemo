7d5872577bbc49ca1fa6d0243b6a65608cdfb20b
Giovanna Cavallaro <cvg@unife.it>
2016-09-24 22:01:23 +0200

    Initial Commit

---
+++ SwishTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwishTest
+//
+//  Created by Giovanna Cavallaro on 24/09/16.
+//  Copyright © 2016 Giovanna Cavallaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
